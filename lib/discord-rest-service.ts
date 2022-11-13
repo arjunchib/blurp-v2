e8edd47d041bae06bf4895556b6928a9511ed467
Arjun Chib <arjunpatrickchib@gmail.com>
2022-11-13 08:10:30 -0500

    Add bulk application command upload

--- lib/discord-rest-service.ts
+++ lib/discord-rest-service.ts
@@ -1,9 +1,20 @@
-import { RESTGetAPIGatewayBotResult } from "discord-api-types";
+import {
+  RESTGetAPIGatewayBotResult,
+  RESTPutAPIApplicationCommandsJSONBody,
+  RESTPutAPIApplicationCommandsResult,
+} from "discord-api-types";
 
 export class DiscordRestService {
   private readonly baseUrl: string;
 
-  constructor(private options: { version: number; token: string }) {
+  constructor(
+    private options: {
+      version: number;
+      token: string;
+      applicationId: string;
+      guildId: string;
+    }
+  ) {
     this.baseUrl = `https://discord.com/api/v${this.options.version}`;
   }
 
@@ -11,6 +22,7 @@ export class DiscordRestService {
     const res = await fetch(`${this.baseUrl}${url}`, {
       headers: {
         Authorization: `Bot ${this.options.token}`,
+        "Content-Type": "application/json",
       },
       ...init,
     });
@@ -19,5 +31,18 @@ export class DiscordRestService {
 
   public async getGatewayBot() {
     return await this.fetch<RESTGetAPIGatewayBotResult>("/gateway/bot");
+  }
+
+  public async bulkOverwriteGuildApplicationCommands(
+    commands: RESTPutAPIApplicationCommandsJSONBody
+  ) {
+    const { applicationId, guildId } = this.options;
+    return await this.fetch<RESTPutAPIApplicationCommandsResult>(
+      `/applications/${applicationId}/guilds/${guildId}/commands`,
+      {
+        body: JSON.stringify(commands),
+        method: "PUT",
+      }
+    );
   }
 }
--- main.ts
+++ main.ts
@@ -2,16 +2,28 @@ import { DiscordGatewayService } from "./lib/discord-gateway-service.ts";
 import { DiscordRestService } from "./lib/discord-rest-service.ts";
 import { configSync } from "https://deno.land/std@0.163.0/dotenv/mod.ts";
 
-const { TOKEN } = configSync();
+const { TOKEN, APPLICATION_ID, GUILD_ID } = configSync();
 
 const API_VERSION = 10;
 
-const context = {
+const options = {
   version: API_VERSION,
   token: TOKEN,
+  applicationId: APPLICATION_ID,
+  guildId: GUILD_ID,
 };
 
-const restService = new DiscordRestService(context);
-const gatewayService = new DiscordGatewayService(context, restService);
+const restService = new DiscordRestService(options);
+
+// console.log(
+//   await restService.bulkOverwriteGuildApplicationCommands([
+//     {
+//       name: "test",
+//       description: "This is a test",
+//     },
+//   ])
+// );
+
+const gatewayService = new DiscordGatewayService(options, restService);
 
 gatewayService.connect();

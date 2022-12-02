192a75349777b36ac176b4da7beecc345132ec97
Arjun Chib <arjunpatrickchib@gmail.com>
2022-12-02 14:10:47 -0500

    Move mod and jsx-runtime to root

---
+++ mod.ts
@@ -0,0 +1,7 @@
+export { DiscoClient } from "./src/client.ts";
+export { start, Interaction } from "./src/disco.ts";
+export type { Command } from "./src/disco.ts";
+export { ChannelMessageWithSource } from "./src/components/channel_message_with_source.ts";
+export { ActionRow } from "./src/components/action_row.ts";
+export { Button } from "./src/components/button.ts";
+export { UpdateMessage } from "./src/components/update-message.ts";
--- src/mod.ts
+++
@@ -1,7 +0,0 @@
-export { DiscoClient } from "./client.ts";
-export { start, Interaction } from "./disco.ts";
-export type { Command } from "./disco.ts";
-export { ChannelMessageWithSource } from "./components/channel_message_with_source.ts";
-export { ActionRow } from "./components/action_row.ts";
-export { Button } from "./components/button.ts";
-export { UpdateMessage } from "./components/update-message.ts";

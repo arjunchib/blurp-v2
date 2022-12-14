98d253cf5dc659fed73712c580ac433715c6c740
Arjun Chib <arjunpatrickchib@gmail.com>
2022-12-13 19:02:31 -0500

    Fix dotenv failing in deno deploy

--- src/environment.ts
+++ src/environment.ts
@@ -1,13 +1,12 @@
-import { configSync } from "./deps.ts";
-
-const { TOKEN, APPLICATION_ID, GUILD_ID, PUBLIC_KEY } = configSync();
+// Auto-load dotenv vars
+import "https://deno.land/std@0.167.0/dotenv/load.ts";
 
 const API_VERSION = 10;
 
 export const environment = {
   version: API_VERSION,
-  token: Deno.env.get("TOKEN") || TOKEN,
-  applicationId: Deno.env.get("APPLICATION_ID") || APPLICATION_ID,
-  guildId: Deno.env.get("GUILD_ID") || GUILD_ID,
-  publicKey: Deno.env.get("PUBLIC_KEY") || PUBLIC_KEY,
+  token: Deno.env.get("TOKEN"),
+  applicationId: Deno.env.get("APPLICATION_ID"),
+  guildId: Deno.env.get("GUILD_ID"),
+  publicKey: Deno.env.get("PUBLIC_KEY"),
 };
